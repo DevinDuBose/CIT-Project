216d3b7ec7f09ade89ced9f1d232e68578e70959
df1217 <dffrank1217@gmail.com>
2017-08-09 13:05:43 -0700

    Add student/instructor/studentregistration script

--- Mock_Data/pop_courses.php
+++ Mock_Data/pop_courses.php
@@ -3,7 +3,30 @@
 //include 'C:\wamp\www\CIT_Remote_Monitoring_App\App\Models\db.php';
 include($_SERVER["DOCUMENT_ROOT"] . "/CIT_Remote_Monitoring_App/App/Models/db.php");
 $lines = file("zsrsecl_cit.txt");
+$studentRegistrationLines = file("zsrslst_cit.txt");
 $db = Database::getDb();
+
+$insert1 = 'INSERT INTO Term (TermID, TermName) Values (0,"Summer")';
+$insert2 = 'INSERT INTO Term (TermID, TermName) Values (1,"Fall")';
+$insert3 = 'INSERT INTO Term (TermID, TermName) Values (2,"Winter")';
+$insert4 = 'INSERT INTO Term (TermID, TermName) Values (3,"Spring")';
+
+$statement = $db->prepare($insert1);
+$statement->execute();
+$statement->closeCursor();
+
+$statement = $db->prepare($insert2);
+$statement->execute();
+$statement->closeCursor();
+
+$statement = $db->prepare($insert3);
+$statement->execute();
+$statement->closeCursor();
+
+$statement = $db->prepare($insert4);
+$statement->execute();
+$statement->closeCursor();
+
 foreach($lines as $line)
 {
 		
@@ -22,11 +45,17 @@ foreach($lines as $line)
 		}
 		
 	}
-
+	$sectionNum = $output[1];
 	$courseNum = $output[2] . " " . $output[3];
 	$courseName = $output[4];
 	$leadInstructor = $output[5];
+	$startDate = $output[9];
+	$termID = 0;
 	
+	$dateParts = explode('-', $startDate);
+	$month = $dateParts[1];
+	$year = $dateParts[2];
+	echo $month . " " . $year . '<br />';
 	
 	
 	
@@ -62,22 +91,92 @@ foreach($lines as $line)
 		$statement->execute();
 		$statement->closeCursor();
 	}
-	/*
-	else
+	
+	switch ($month)
 	{
-		if (strpos($email, 'lanecc.edu'))
-		{
-			$query3 = 'UPDATE appuser SET EmailAddress = :email WHERE LNumber = :lnum';
-			$statement = $db->prepare($query3);
-			$statement->bindValue(':lnum', $lnum);
-			$statement->bindValue(':email', $email);
-			$statement->execute();
-			$statement->closeCursor();
+		case "JUN":
+			$termID = 0;
+			break;
+		case "SEP":
+			$termID = 1;
+			break;
+		case "JAN":
+			$termID = 2;
+			break;
+		case "APR":
+			$termID = 3;
+			break;
+	}
+	
+	$query4 = 'INSERT INTO section (CourseNumber, SectionNumber, TermId, UserID, Year) 
+							VALUES(:coursenum, :sectionnum, :termid, :instructor, :year)';
+	
+	$statement = $db->prepare($query4);
+	$statement->bindValue(':coursenum', $courseNum);
+	$statement->bindValue(':sectionnum', $sectionNum);
+	$statement->bindValue(':termid', $termID);
+	$statement->bindValue(':instructor', $userID);
+	$statement->bindValue(':year', $year);
+	$statement->execute();
+	$statement->closeCursor();
+
+		
+}
+
+echo count($studentRegistrationLines) . "<br />";
+foreach ($studentRegistrationLines as $line)
+{
+	$trim_line = preg_replace('/"\s+"/', '""', $line);
+	echo $trim_line . "<br/>";
+	$parts = explode('""', $trim_line);
+	$output = array();
+	foreach ($parts as $part)
+	{
+		$trim_part = preg_replace('/"/', '', $part);
+		echo $trim_part . "<br />";
 			
+		if ( $trim_part != "")
+		{
+			array_push($output, $trim_part);			
 		}
+		
+	}
+	$lnum = $output[0];
+	$sectionNum = $output[1];
+	
+	$query5 = 'SELECT UserID FROM AppUser WHERE LNumber = :lnum';
+	$statement = $db->prepare($query5);
+	$statement->bindValue(':lnum', $lnum);
+	$statement->execute();
+	$rows = $statement->fetch();
+	$statement->closeCursor();
+	$userID = $rows[0];
+	//$second = $rows[1];
+	echo $userID . "<br />" . count($rows) . "<br />" .  "<br />";
+	
+	$query6 = 'INSERT INTO studentregistration (SectionNumber, UserID)
+							VALUES(:sectionnum, :userid)';
+	$statement = $db->prepare($query6);
+	$statement->bindValue(':sectionnum', $sectionNum);
+	$statement->bindValue(':userid', $userID);
+	$statement->execute();
+	$statement->closeCursor();
+	
+	$query7 = 'SELECT * FROM studentregistration';
+	$statement = $db->prepare($query7);
+	$statement->execute();
+	$statement->closeCursor();
+	$rows = $statement->fetchAll();
+	
+	foreach ($rows as $row)
+	{
+		echo count($row) . "<br />" . "<br />";
+		echo $row[0] . "<br />" . $row[1] . "<br />";
+	
 	}
 		
-	*/		
+	//echo $sectionNum . "<br />" . $userID . "<br />";
 	
 }
+
 ?>
